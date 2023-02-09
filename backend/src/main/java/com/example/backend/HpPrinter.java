8e271b26cac58e045f9520d654c5632c9c6b4f91
elephant82 <124107611+elephant82@users.noreply.github.com>
2023-02-09 11:03:10 +0800

    git commit

---
+++ backend/src/main/java/com/example/backend/HpPrinter.java
@@ -0,0 +1,15 @@
+package com.example.backend;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HpPrinter implements Printer {
+
+    @Override
+    public void print(String message) {
+        
+        System.out.println("HP印表機"+message);
+    }
+    
+    
+}
